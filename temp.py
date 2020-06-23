87ea24c1c1c2259da346b13b5020549d36fff35f
Yan-GH <60241112+Yan-GH@users.noreply.github.com>
2020-06-23 11:47:11 -0700

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder 编辑器
+
+这是一个临时脚本文件。
+"""
+
+import sys
+
+import tensorflow.keras
+import pandas as pd
+import sklearn as sk
+import tensorflow as tf
+
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tensorflow.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+print(f"Pandas {pd.__version__}")
+print(f"Scikit-Learn {sk.__version__}")
+print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
