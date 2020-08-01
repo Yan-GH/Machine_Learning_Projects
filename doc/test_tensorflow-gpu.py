b418e32e2d2008f7013ef273cbefd35b2a075432
Yan-GH <60241112+Yan-GH@users.noreply.github.com>
2020-08-01 01:38:38 -0700

    test if tensorflow-gpu is working

---
+++ doc/test_tensorflow-gpu.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+import tensorflow.keras
+import pandas as pd
+import sklearn as sk
+import tensorflow as tf
+
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tensorflow.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+print(f"Pandas {pd.__version__}")
+print(f"Scikit-Learn {sk.__version__}")
+print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
